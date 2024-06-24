27a157755642dcef029fa44e24ee73c5868b09d2
Abd Elrhman Mohamed Shata <abdelrhmanmohamedshata@gmail.com>
2024-06-24 19:41:13 +0300

    Update Project
    Connect To MangoDB and use it

--- ProjectNode/controllers/controller.js
+++ ProjectNode/controllers/controller.js
@@ -1,47 +1,81 @@
 const { validationResult } = require("express-validator");
-let { courses } = require("../data/courses");
+// let { courses } = require("../data/courses");
 
-const getAllCourses = (req, res) => {
+const Course = require("../models/course.model");
+
+const getAllCourses = async (req, res) => {
+  // get all courses from the database using the Course model
+  const courses = await Course.find();
+  console.log(courses);
   res.json(courses);
 };
 
-const getCourse = (req, res) => {
-  const courseID = +req.params.courseID;
-  const course = courses.find((course) => course.id === courseID);
-  if (!course) {
-    return res.status(404).json({
-      msg: "Course not found",
+const getCourse = async (req, res) => {
+  // const courseID = +req.params.courseID;
+  // const course = courses.find((course) => course.id === courseID);
+
+  Course.findById(req.params.courseID)
+    .then((course) => {
+      if (!course) {
+        return res.status(404).json({
+          msg: "Course not found",
+        });
+      }
+      return res.json(course);
+    })
+    .catch((err) => {
+      console.log("Error : ", err);
+      return res.status(400).json({
+        msg: "Invalid Object ID",
+      });
     });
-  }
-  res.json(course);
 };
 
-const createCourse = (req, res) => {
+const createCourse = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
-  const course = { id: courses.length + 1, ...req.body };
-  courses.push(course);
-  res.status(201).json(course);
+  // const course = { id: courses.length + 1, ...req.body };
+  // courses.push(course);
+
+  const newCourse = new Course(req.body);
+  await newCourse.save();
+  res.status(201).json(newCourse);
 };
 
-const updateCousre = (req, res) => {
-  const courseID = +req.params.courseID;
-  let course = courses.find((course) => course.id == courseID);
-  if (!course) {
-    return res.status(404).json({
-      msg: "Course not found",
-    });
+const updateCousre = async (req, res) => {
+  const courseID = req.body.courseID;
+  try {
+    const updateCourse = await Course.updateOne(
+      { _id: courseID },
+      {
+        $set: { ...req.body },
+      }
+    );
+    return res.status(200).json(updateCourse);
+  } catch (err) {
+    return res.status(400).json({ "Error : ": err });
   }
-  course = { ...course, ...req.body };
-  res.json(course);
+
+  // const courseID = +req.params.courseID;
+  // let course = courses.find((course) => course.id == courseID);
+  // if (!course) {
+  //   return res.status(404).json({
+  //     msg: "Course not found",
+  //   });
+  // }
+  // course = { ...course, ...req.body };
+  // res.json(course);
 };
 
-const deleteCousre = (req, res) => {
-  const courseID = +req.params.courseID;
-  courses = courses.filter((course) => course.id !== courseID);
-  res.status(200).json({ success: true, msg: "Course deleted" });
+const deleteCousre = async (req, res) => {
+  const result = await Course.deleteOne({ _id: req.params.courseID });
+  res.status(200).json({ success: true, msg: result });
+
+  // const courseID = +req.params.courseID;
+  // courses = courses.filter((course) => course.id !== courseID);
+  // res.status(200).json({ success: true, msg: "Course deleted" });
 };
 
 module.exports = {
--- ProjectNode/index.js
+++ ProjectNode/index.js
@@ -2,8 +2,29 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const mongoose = require("mongoose");
+const url =
+  "mongodb+srv://abdelrhmanmohamedshata:AbdElrhman@mango-db.65s9p3e.mongodb.net/codeZone?retryWrites=true&w=majority&appName=mango-db";
+mongoose
+  .connect(url)
+  .then((res) => {
+    console.log("Connected to MongoDB Successfully");
+  })
+  .catch((err) => {
+    console.log("Error : ", err);
+  });
+
+// const main = async () => {
+//   await client.connect(); // connect to MongoDB
+//   console.log("Connected to MongoDB Successfully");
+//   const db = client.db("codeZone"); // choose database to interact with
+//   const collection = db.collection("courses"); // choose collection to interact with
+//   const data = await collection.find().toArray(); // get query data from collection
+//   console.log("Data", data);
+// };
+
 const coursesRouter = require("./routes/route");
-app.use('/api/courses',coursesRouter); // localhost / => /api/courses   routes
+app.use("/api/courses", coursesRouter); // localhost / => /api/courses   routes
 
 app.listen(5000, () => {
   console.log("Example app listening on port 5000!");
